893ca92d136ecc835eba381d77a06fcf893e6edd
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-16 18:51:45 +0530

    Added basic home page layout, RootLayout component with metadata, and implemented About and Profile pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+	// title: "About Codevolution",
+	title: {
+		absolute: "About Codevolution",
+	},
+}
+
+
+export default function About() {
+	return <h1>About me</h1>;
+}
