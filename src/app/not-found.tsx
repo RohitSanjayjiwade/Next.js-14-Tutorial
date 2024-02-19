06c3b4b4f0df987b997d2277de9262bc768c6da0
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-19 13:38:17 +0530

    Add Not Found page component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+export default function NotFound() {
+
+
+
+	return (
+		<div> 
+			<h2>Page Not Found</h2>
+			<p>Could not find requested resource</p>
+		</div>
+	);
+}
+
+// state = {
+//     1: {
+//         "name": "MH",
+//         "d_list": [{"name": "Jalgaon", "city_list": ["a", "b"]}, "P"]
+//     },
+//     2: {
+//         "name": "MP",
+//         "d_list": ["A", "B"]
+//     },
+//     3: {
+//         "name": "JK",
+//         "d_list": ["C", "D"]
+//     },
+// }
+
+// slug = 3
